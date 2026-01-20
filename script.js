aab54707881a694558f00ce498fc26d1035ef516
Jilika Deekshitha Sri <147692729+Deekshitha1035@users.noreply.github.com>
2026-01-20 16:12:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,53 @@
-// access input field
-const input = document.querySelector('#todo-input');
+const input = document.getElementById("todo-input");
+const submitBtn = document.getElementById("submit");
+const todoLists = document.getElementById("todo-lists");
 
-// Listening to click event from "Add" button.
-document.querySelector('#submit').addEventListener('click', () => {
-  // value of the input field
-  const inputData = input.value;
+submitBtn.addEventListener("click", addTodo);
+
+function addTodo() {
+  if (input.value.trim() === "") return;
+
+  const todoItem = document.createElement("div");
+  todoItem.classList.add("todo-item");
+
+  const textInput = document.createElement("input");
+  textInput.type = "text";
+  textInput.value = input.value;
+  textInput.classList.add("todo-text");
+  textInput.setAttribute("readonly", "readonly");
+
+  const actions = document.createElement("div");
+  actions.classList.add("action-items");
+
+  const check = document.createElement("i");
+  check.className = "fa-solid fa-check";
+
+  const edit = document.createElement("i");
+  edit.className = "fa-solid fa-pen-to-square";
+
+  const trash = document.createElement("i");
+  trash.className = "fa-solid fa-trash";
+
+  actions.append(check, edit, trash);
+  todoItem.append(textInput, actions);
+  todoLists.append(todoItem);
+
   input.value = "";
 
-  // creating todo item element
-  const todo_el = document.createElement('div');
-  todo_el.classList.add('todo-item');
+  check.addEventListener("click", () => {
+    textInput.classList.toggle("completed");
+  });
 
-  const todo_content_el = document.createElement('div');
-  todo_el.appendChild(todo_content_el);
-
-  const todo_input_el = document.createElement('input');
-  todo_input_el.classList.add('text');
-  todo_input_el.type = 'text';
-  todo_input_el.value = inputData;
-  todo_input_el.setAttribute('readonly', 'readonly');
-
-  todo_content_el.appendChild(todo_input_el);
-
-  const todo_actions_el = document.createElement('div');
-  todo_actions_el.classList.add('action-items');
-
-  const todo_done_el = document.createElement('i');
-  todo_done_el.classList.add('fa-solid');
-  todo_done_el.classList.add('fa-check');
-
-  const todo_edit_el = document.createElement('i');
-  todo_edit_el.classList.add('fa-solid');
-  todo_edit_el.classList.add('fa-pen-to-square');
-  todo_edit_el.classList.add('edit');
-
-  const todo_delete_el = document.createElement('i');
-  todo_delete_el.classList.add('fa-solid');
-  todo_delete_el.classList.add('fa-trash');
-
-  todo_actions_el.appendChild(todo_done_el)
-  todo_actions_el.appendChild(todo_edit_el);
-  todo_actions_el.appendChild(todo_delete_el);
-
-  todo_el.appendChild(todo_actions_el);
-  console.log(todo_el)
-  // add the todo-item to lists
-  document.querySelector('.todo-lists').appendChild(todo_el);
-
-  // done functionality
-  todo_done_el.addEventListener('click', () => {
-    todo_input_el.classList.add('done')
-    todo_el.removeChild(todo_actions_el);
-  })
-
-  // edit functionality
-  todo_edit_el.addEventListener('click', (e) => {
-    if (todo_edit_el.classList.contains("edit")) {
-      todo_edit_el.classList.remove("edit");
-      todo_edit_el.classList.remove("fa-pen-to-square");
-      todo_edit_el.classList.add("fa-x");
-      todo_edit_el.classList.add("save");
-      todo_input_el.removeAttribute("readonly");
-      todo_input_el.focus();
+  edit.addEventListener("click", () => {
+    if (textInput.hasAttribute("readonly")) {
+      textInput.removeAttribute("readonly");
+      textInput.focus();
     } else {
-      todo_edit_el.classList.remove("save");
-      todo_edit_el.classList.remove("fa-x");
-      todo_edit_el.classList.add("fa-pen-to-square");
-      todo_edit_el.classList.add("edit");
-      todo_input_el.setAttribute("readonly", "readonly");
+      textInput.setAttribute("readonly", "readonly");
     }
   });
 
-  // delete functionality
-  todo_delete_el.addEventListener('click', (e) => {
-    console.log(todo_el);
-    document.querySelector('.todo-lists').removeChild(todo_el);
+  trash.addEventListener("click", () => {
+    todoItem.remove();
   });
-})
+}
